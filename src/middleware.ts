47de5b492e650f7dab8c4b6bb433e65dfac17fc3
Martin Lapsansky <martin.lapsa2@gmail.com>
2026-01-31 13:12:35 +0100

    edited json

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,19 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+// middleware.ts
+import { withAuth } from "next-auth/middleware";
 
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/admin")) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
-    if (!token) {
-      const url = req.nextUrl.clone();
-      url.pathname = "/auth/signin";
-      url.searchParams.set("callbackUrl", req.nextUrl.pathname);
-      return NextResponse.redirect(url);
-    }
-
-    const role = token.role as string | undefined;
-    const allowed = role === "SUPERADMIN" || role === "ADMIN" || role === "BARBER";
-
-    if (!allowed) {
-      const url = req.nextUrl.clone();
-      url.pathname = "/customer"; // alebo "/" podľa toho čo chceš
-      return NextResponse.redirect(url);
-    }
-  }
-
-  return NextResponse.next();
-}
+export default withAuth({
+    callbacks: {
+        authorized: ({ token }) => {
+            if (!token) return false;
+            const role = token.role;
+            return ["SUPERADMIN", "ADMIN", "COACH"].includes(role);
+        },
+    },
+    pages: {
+        signIn: "/auth/signin",
+    },
+});
 
 export const config = {
-  matcher: ["/admin/:path*"],
+    matcher: ["/admin/:path*"],
 };
